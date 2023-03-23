3c3ede9c5688ef3de0937d78d8f84a15ff0dc9e6
MitaliPadhiya <122818704+jajosh777@users.noreply.github.com>
2023-03-23 15:04:34 -0400

    latest update
    latest update

--- lab6_script_template.py
+++ lab6_script_template.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 import subprocess
+import requests
 
 def main():
 
@@ -24,21 +25,31 @@ def main():
         delete_installer(installer_path)
 
 def get_expected_sha256():
-    return 
+    link = ' http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe.sha256'
+    resp_msg = requests.get(link)
+    return resp_msg
 
 def download_installer():
-    return
+    installer_link = 'http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe'
+    resp_msg = requests.get(installer_link)
+    return resp_msg
 
 def installer_ok(installer_data, expected_sha256):
+    hash_img=hashlib.sha256(file_content).hexadigest()
+    expected_sha256=hash_img
+    installer_data(expected_sha256)
+    print(hash_img)
     return
 
 def save_installer(installer_data):
     return
 
 def run_installer(installer_path):
+    subprocess.run([installer_path, '/L=1033', '/S'])
     return
     
 def delete_installer(installer_path):
+    os.remove(installer_path)
     return
 
 if __name__ == '__main__':
